9218f08c4ab9bc04bca9ddaaefe82fc59356d8b0
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2026-02-16 18:05:24 +0100

    added debouncing

--- macropad.ino
+++ macropad.ino
@@ -1,11 +1,37 @@
-void buttonHandler(void* pin) {
-  Serial.print("Interrupt ");
-  Serial.println(reinterpret_cast<int>(pin));
+#include "Arduino.h"
+
+
+constexpr static uint32_t debounceTime {20};
+
+uint32_t buttonTimes[15] {0};
+
+
+void buttonHandler(void* pinPtr) {
+  uint8_t pin {static_cast<uint8_t>(reinterpret_cast<int>(pinPtr))};
+
+  auto buttonTime = buttonTimes[pin];
+  buttonTimes[pin] = millis();
+
+  if (millis() - buttonTime < debounceTime) {
+    return;
+  }
+
+  Serial.print("Button ");
+  Serial.println(static_cast<int>(pin));
 }
 
-void encoderHandler(void* pin) {
-  Serial.print("Interrupt ");
-  Serial.println(reinterpret_cast<int>(pin));
+void encoderHandler(void* pinPtr) {
+  uint8_t pin {static_cast<uint8_t>(reinterpret_cast<int>(pinPtr) - 17)};
+
+  auto buttonTime = buttonTimes[pin];
+  buttonTimes[pin] = millis();
+
+  if (millis() - buttonTime < debounceTime) {
+    return;
+  }
+
+  Serial.print("Encoder ");
+  Serial.println(static_cast<int>(pin));
 }
 
 void setup() {
