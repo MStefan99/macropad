671d1b60ab4b305766102ba8fc6315e174a942f9
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2026-02-16 22:01:53 +0100

    simple keyboard setup

--- macropad.ino
+++ macropad.ino
@@ -1,4 +1,5 @@
 #include "Arduino.h"
+#include "Keyboard.h"
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
@@ -11,8 +12,9 @@
 #define OLED_CS       22
 #define OLED_RST      23
 
-#define LED_PIN    19
-#define LED_COUNT 12
+#define LED_PIN 13
+#define NEOPIXEL_PIN    19
+#define NEOPIXEL_COUNT 12
 
 
 constexpr static uint32_t buttonDebounceTime {20};
@@ -27,7 +29,7 @@ constexpr static int8_t encoderTransitions[4][4] {
 };
 
 static Adafruit_SH1106G display = Adafruit_SH1106G(128, 64, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RST, OLED_CS);
-static Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+static Adafruit_NeoPixel strip(NEOPIXEL_COUNT, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 static PinStatus pinStatuses[13] {};
 static uint8_t encoderPosition {0};
 static int32_t encoderCount {0};
@@ -44,30 +46,26 @@ void buttonHandler(void* pinPtr) {
 
   strip.setPixelColor(pin - 1, status? 0 : 0xffffff);
 
-  char buf[64];
-  snprintf(buf, sizeof(buf), "Button %i %i\n", pin, status);
-  Serial.print(buf);
+  if (status == PinStatus::LOW) {
+    Keyboard.press('a' + pin);
+  } else {
+    Keyboard.release('a' + pin);
+  }
 }
 
 void encoderHandler(void* pinPtr) {
-  uint8_t pin {static_cast<uint8_t>(reinterpret_cast<int>(pinPtr) - 4)};
-  auto status = digitalRead(pin);
-
   uint8_t newPosition = (static_cast<uint8_t>(digitalRead(17)) << 1) | (static_cast<uint8_t>(digitalRead(18)));
   encoderCount -= encoderTransitions[encoderPosition][newPosition];
   encoderPosition = newPosition;
-
-  char buf[64];
-  snprintf(buf, sizeof(buf), "Encoder %i\n", encoderCount);
-  Serial.print(buf);
 }
 
 void setup() {
-  for (uint8_t i {}; i < sizeof(pinStatuses) / sizeof(pinStatuses[0]); ++i) {
-    pinStatuses[i] = PinStatus::HIGH;
-  }
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
 
-  Serial.begin(115200);
+  for (uint8_t i {}; i < sizeof(pinStatuses) / sizeof(pinStatuses[0]); ++i) {
+    pinStatuses[i] = digitalRead(i);
+  }
 
   for (uint8_t i {0}; i <= 12; ++i) {
     pinMode(i, INPUT_PULLUP);
@@ -79,13 +77,20 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(i), encoderHandler, PinStatus::CHANGE, reinterpret_cast<void*>(i));
   }
 
+  Serial.begin(115200);
+
+  Keyboard.begin();
+
   strip.begin();
   strip.show();
+
   display.begin(0, true);
   display.display();
   delay(2000);
   display.clearDisplay();
   display.display();
+
+  digitalWrite(LED_PIN, LOW);
 }
 
 void loop() {
