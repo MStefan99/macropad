172851c14c175c28671ecbbdc6a33d3ca528cf8e
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2026-02-16 19:07:43 +0100

    added OLED init

--- macropad.ino
+++ macropad.ino
@@ -1,4 +1,14 @@
 #include "Arduino.h"
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
+
+#define OLED_MOSI     27
+#define OLED_CLK      26
+#define OLED_DC       24
+#define OLED_CS       22
+#define OLED_RST      23
 
 
 constexpr static uint32_t buttonDebounceTime {20};
@@ -12,6 +22,7 @@ constexpr static int8_t encoderTransitions[4][4] {
   { 0,  1, -1,  0}
 };
 
+static Adafruit_SH1106G display = Adafruit_SH1106G(128, 64, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RST, OLED_CS);
 static uint32_t buttonTimes[14] {0};
 static uint8_t encoderPosition = 0;
 static int32_t encoderCount {0};
@@ -42,6 +53,9 @@ void encoderHandler(void* pinPtr) {
   uint8_t newPosition = (static_cast<uint8_t>(digitalRead(17)) << 1) | (static_cast<uint8_t>(digitalRead(18)));
   encoderCount -= encoderTransitions[encoderPosition][newPosition];
   encoderPosition = newPosition;
+
+  Serial.print("Encoder ");
+  Serial.println(encoderCount);
 }
 
 void setup() {
@@ -57,6 +71,9 @@ void setup() {
     pinMode(i, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(i), encoderHandler, PinStatus::CHANGE, reinterpret_cast<void*>(i));
   }
+
+  display.begin(0, true);
+  display.display();
 }
 
 void loop() {
