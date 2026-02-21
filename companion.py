46f4a122be3aaf082527d8ad5ec6b3bf12806f5d
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2026-02-21 12:31:52 +0100

    hot plugging support

--- companion.py
+++ companion.py
@@ -1,8 +1,10 @@
 import subprocess
 import serial
 import time
+import os
 
 current_app = ""
+port = None
 
 
 def get_active_window_title():
@@ -18,7 +20,7 @@ def get_active_window_title():
     return None
 
 
-def send_over_serial(title, port):
+def send_over_serial(title):
   try:
     command = f"a>{title}"
     port.write(command.encode('utf-8'))
@@ -26,8 +28,8 @@ def send_over_serial(title, port):
     print(f"Opening {title}")
 
     time.sleep(0.1)
-    if ser.in_waiting > 0:
-      response = ser.read(ser.in_waiting).decode('utf-8', errors='ignore').strip();
+    if port.in_waiting > 0:
+      response = port.read(port.in_waiting).decode('utf-8', errors='ignore').strip();
       print(f"Received from macropad: {response}")
 
   except serial.SerialException as e:
@@ -41,19 +43,33 @@ if __name__ == "__main__":
   BAUD_RATE = 115200
   SLEEP_INTERVAL = 1
 
-  ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
-
   try:
     while True:
-      window_title = get_active_window_title()
+      if not port:
+        try:
+          if os.path.exists(SERIAL_PORT):
+            port = serial.Serial(SERIAL_PORT, BAUD_RATE)
+            print(f"Connected to {SERIAL_PORT}")
+        except Exception as e:
+          printf(f"Could not connect to {SERIAL_PORT}: {e}")
+          pass
 
-      if window_title:
-        if (window_title != current_app):
-          current_app = window_title
-          send_over_serial(window_title, ser)
+      elif not os.path.exists(SERIAL_PORT):
+        port.close()
+        port = None
+        current_app = ""
+        print(f"Disconnected from {SERIAL_PORT}")
+
+      else:
+        window_title = get_active_window_title()
+
+        if window_title:
+          if (window_title != current_app):
+            current_app = window_title
+            send_over_serial(window_title)
 
       time.sleep(SLEEP_INTERVAL)
 
   except Exception as e:
     print(f"An unexpected error occurred: {e}")
-    ser.close()
+    port.close()
