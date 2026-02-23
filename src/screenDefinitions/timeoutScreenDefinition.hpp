515cee46353a0c4a20ea7d0c82230d20362341f0
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2026-02-23 21:14:00 +0100

    adjusted timeouts

--- src/screenDefinitions/timeoutScreenDefinition.hpp
+++ src/screenDefinitions/timeoutScreenDefinition.hpp
@@ -10,12 +10,14 @@
 SelectScreen::Definition timeoutScreenDefinition {
   "s_timeout",
   "Timeout",
-  {{"10s", 1000 * 10},
+  {{"15s", 1000 * 15},
     {"30s", 1000 * 30},
     {"1m", 1000 * 60},
+    {"3m", 1000 * 60 * 3},
     {"5m", 1000 * 60 * 5},
     {"10m", 1000 * 60 * 10},
-    {"30m", 1000 * 60 * 30}},
+    {"30m", 1000 * 60 * 30},
+    {"1h", 1000 * 60 * 60}},
   []() -> int {
 	return settingsProvider::getSettings().screenTimeout;
     },
